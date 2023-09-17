7f96d77950ba41a7e6c6624cc2e085831ff990e7
Batuhan <kucukyildizbatuhan@gmail.com>
2023-09-17 11:47:05 +0300

    Initial Commit

---
+++ TwitchClone/TwitchCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitchCloneApp.swift
+//  TwitchClone
+//
+//  Created by Batuhan Küçükyıldız on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitchCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
